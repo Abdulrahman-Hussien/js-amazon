738c9e6a74f6fc4988994cfe23b4fcb5194b4021
Abdul <abdulrahman.hussien1999@gmail.com>
2024-09-29 15:54:27 +0300

    Make Delivery buttons interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, cartQuantity, removeFromCart, updateQuantity } from '../data/cart.js';
+import {cart, cartQuantity, removeFromCart, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import formatCurrency from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -87,7 +87,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
-      html += `<div class="delivery-option">
+      html += `<div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -160,3 +162,11 @@ document.querySelectorAll('.js-save-link')
       updateNewQuantity(productId);
     });
   });
+
+  document.querySelectorAll('.js-delivery-option')
+    .forEach(element =>{
+      const {productId, deliveryOptionId} = element.dataset;
+      element.addEventListener('click', () => {
+        updateDeliveryOption(productId, deliveryOptionId);
+      });
+    });
